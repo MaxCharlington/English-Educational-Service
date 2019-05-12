cc79438e84f098282653448f738148144f4b5a6a
MaxCharlington <maxcharlington03@yandex.ru>
2019-05-13 00:54:48 +0300

    Sha256 added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
-//import $ from "jquery";
+import $ from "jquery";
 //import lib from './lib.js';
 import '../sass/styles.scss';
+var sha256 = require("js-sha256");
 
 function b() { 
-    let a = () => {console.log("Hello")}; 
+    let a = () => {console.log(sha256("Hello"))}; 
     a();
 }
 
